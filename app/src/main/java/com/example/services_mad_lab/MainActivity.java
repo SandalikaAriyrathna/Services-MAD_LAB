a67a9cc587a0b93375d682c1494f3c5acf5ceaf8
B.M.N.D.S.Ariyarathnna <it20643836@my.sliit.lk>
2022-08-19 08:23:04 +0530

    completed

--- app/src/main/java/com/example/services_mad_lab/MainActivity.java
+++ app/src/main/java/com/example/services_mad_lab/MainActivity.java
@@ -2,22 +2,50 @@ package com.example.services_mad_lab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MediaPlayer player =
-                MediaPlayer.create(this, Settings.System.DEFAULT_NOTIFICATION_URI);
-        player.setLooping(true);
-        player.start();
-    }
+
+        Button btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+
+
+
+
+        Button btnStop = findViewById(R.id.btnStop);
+
+
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+        }
+
+
+
 
 
 }
+
+
